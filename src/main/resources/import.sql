c2952e939d7eea3009f14496a5c9b451fcd4aacb
Diego Sousa <me@diego.dev.br>
2025-07-06 00:07:51 -0300

    adicionando import

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS (username, password, role) values ('ana@email.com', '123456', 'ROLE_ADMIN');
+insert into USUARIOS (username, password, role) values ('bia@email.com', '123456', 'ROLE_CLIENTE');
+insert into USUARIOS (username, password, role) values ('bob@email.com', '123456', 'ROLE_CLIENTE');
