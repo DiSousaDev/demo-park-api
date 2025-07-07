2ee37f28960538bcfc1ad507365a49100be17505
Diego Sousa <me@diego.dev.br>
2025-07-06 23:09:50 -0300

    Adicionando AutenticacaoController

---
+++ src/main/java/br/dev/diego/demoparkapi/web/controller/AutenticacaoController.java
@@ -0,0 +1,54 @@
+package br.dev.diego.demoparkapi.web.controller;
+
+import br.dev.diego.demoparkapi.config.jwt.JwtToken;
+import br.dev.diego.demoparkapi.config.jwt.JwtUserDetailsService;
+import br.dev.diego.demoparkapi.web.dto.UsuarioLoginDto;
+import br.dev.diego.demoparkapi.web.exception.ErrorMessage;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/auth")
+public class AutenticacaoController {
+
+    private static final Logger LOGGER = Logger.getLogger(AutenticacaoController.class.getName());
+
+    private final JwtUserDetailsService jwtUserDetailsService;
+    private final AuthenticationManager authenticationManager;
+
+    public AutenticacaoController(JwtUserDetailsService jwtUserDetailsService, AuthenticationManager authenticationManager) {
+        this.jwtUserDetailsService = jwtUserDetailsService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> autenticar(@RequestBody @Valid UsuarioLoginDto usuarioLoginDto, HttpServletRequest request) {
+        LOGGER.info("Iniciando autenticação para o usuário: " + usuarioLoginDto.username());
+
+        try {
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    usuarioLoginDto.username(), usuarioLoginDto.password());
+            authenticationManager.authenticate(authenticationToken);
+
+            JwtToken jwtToken = jwtUserDetailsService.getTokenAuthenticated(usuarioLoginDto.username());
+            LOGGER.info("Autenticação bem-sucedida para o usuário: " + usuarioLoginDto.username());
+            return ResponseEntity.ok(jwtToken);
+        } catch (AuthenticationException e) {
+            LOGGER.severe("Falha na autenticação para o usuário: " + usuarioLoginDto.username() + " - " + e.getMessage());
+        }
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Usuário ou senha inválidos"));
+    }
+}
---
+++ src/main/java/br/dev/diego/demoparkapi/web/dto/UsuarioLoginDto.java
@@ -0,0 +1,15 @@
+package br.dev.diego.demoparkapi.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UsuarioLoginDto(
+        @NotBlank
+        @Email(message = "formato do e-mail está invalido", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+        String username,
+        @NotBlank
+        @Size(min = 6, max = 6)
+        String password
+) {
+}
